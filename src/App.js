44f8acebff6e66b577c8180ab10998df71610282
dankkunal <kunalbehlba@gmail.com>
2023-06-09 13:25:39 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import "./App.css";
 
 function App() {
+  const [date, setDate] = useState("");
+  const [age, setAge] = useState(0);
+  const dateInputRef = useRef(null);
+  const handleChange = (e) => {
+    e.preventDefault();
+    setDate(e.target.value);
+  };
+  const calculateAge = (date) => {
+    const today = new Date().getTime();
+    const birthDate = new Date(date).getTime();
+    const age = Math.floor((today - birthDate) / (1000 * 60 * 60 * 24 * 365));
+    setAge(age);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Age Calculator</h1>
+        <h3>Enter your date of birth</h3>
+        <div className="Age-Container">
+          <input type="date" onChange={handleChange} ref={dateInputRef} />
+          <button onClick={() => calculateAge(date)}>Calculate Age</button>
+          {age === 0 ? (
+            <h3>Enter Date to calculate your age</h3>
+          ) : (
+            <h3>You are {age} years old</h3>
+          )}
+        </div>
       </header>
     </div>
   );
